86572eef440e85d2a362ad52147243b55af998ee
Joshua Ledbetter <jledbetter@amherst.com>
2020-06-09 14:13:54 -0500

    adds create index.js functionality

--- bin/index.js
+++ bin/index.js
@@ -7,6 +7,8 @@ var capitalize = require("../lib/capitalize");
 
 const yargs = require("yargs");
 
+const generateIndexFile = true;
+
 const options = yargs.usage("Usage: -n <name>").option("deleteOriginals", {
   alias: "d",
   describe: "should delete original files",
@@ -41,37 +43,69 @@ function deleteFile(filename) {
   });
 }
 
+function fileIsDir(file) {
+  return file.indexOf(".") === -1;
+}
+
+function getComponentName(file, dir) {
+  const dirs = dir.split("/");
+  const prevDir = dirs[dirs.length - 1];
+  return `${capitalize(prevDir).replace(/ /g, "")}${capitalize(
+    file.replace(".svg", "")
+  ).replace(/ /g, "")}`;
+}
+
 function read(dir) {
   if (dir.indexOf("node_modules") > -1) return;
-  fs.readdir(dir, function (err, files) {
+  fs.readdir(dir, async function (err, files) {
     //handling error
     if (err) {
       return console.log("Unable to scan directory: " + err);
     }
     //listing all files using forEach
+    const dirList = [];
+    const fileList = [];
     files.forEach(async function (file) {
-      if (file.indexOf(".") === -1) {
+      if (fileIsDir(file)) {
         return read(path.join(dir, file));
       }
+
       if (file.indexOf(".svg") > -1) {
         try {
+          console.log("dir", dir);
+          dirList.push(dir);
+          fileList.push(file.replace(/ /g, ""));
           const currentContents = await readFileAsync(path.join(dir, file));
           await writeFileAsync(
-            path.join(dir, file).replace(".svg", ".jsx"),
-            StringToJSX(
-              currentContents,
-              capitalize(file.replace(".svg", "")).replace(" ", "")
-            )
+            path.join(dir, file).replace(/ /g, "").replace(".svg", ".jsx"),
+            StringToJSX(currentContents, getComponentName(file, dir))
           );
           if (options.deleteOriginals) {
             await deleteFile(path.join(dir, file));
           }
+          console.log(path.join(dir, file));
         } catch (e) {
           console.error(e);
         }
       }
-      console.log(path.join(dir, file));
     });
+
+    if (generateIndexFile) {
+      dirList.forEach(async function (directory) {
+        await writeFileAsync(
+          path.join(directory, "index.js"),
+          fileList
+            .map(
+              (file) =>
+                `export { default as ${getComponentName(
+                  file,
+                  directory
+                )} } from './${file.replace(".svg", "")}';\n`
+            )
+            .join(" ")
+        );
+      });
+    }
   });
 }
 
